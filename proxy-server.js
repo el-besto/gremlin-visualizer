8a37b9f983caabd15022e993abadbf2351a47697
Angelo Cisneros <a.e.cisneros@gmail.com>
2020-06-25 16:13:59 -0400

    CH proxy port to 5000

--- proxy-server.js
+++ proxy-server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const gremlin = require('gremlin');
 const cors = require('cors');
 const app = express();
-const port = 3001;
+const port = 5000;
 
 app.use(cors({
   credentials: true,
