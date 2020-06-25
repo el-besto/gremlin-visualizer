3bbe555c929fc37c51cbb86557c58a641df9e5bc
Angelo Cisneros <a.e.cisneros@gmail.com>
2020-06-25 16:16:36 -0400

    RM hard-coded SERVER_URL & simplify QUERY_ENDPOINT

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
-const SERVER_URL = 'http://localhost:3001';
-export const QUERY_ENDPOINT = `${SERVER_URL}/query`;
+export const QUERY_ENDPOINT = `/query`;
 export const COMMON_GREMLIN_ERROR = 'Invalid query. Please execute a query to get a set of vertices';
 export const ACTIONS = {
   SET_HOST: 'SET_HOST',
