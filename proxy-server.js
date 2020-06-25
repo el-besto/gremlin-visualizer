4f1380d56ca0f1d1ac4348a8c28c53a2d3cff878
Angelo Cisneros <a.e.cisneros@gmail.com>
2020-06-25 16:15:07 -0400

    Use static dir build

--- proxy-server.js
+++ proxy-server.js
@@ -4,6 +4,13 @@ const gremlin = require('gremlin');
 const cors = require('cors');
 const app = express();
 const port = 5000;
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.use(cors({
   credentials: true,
