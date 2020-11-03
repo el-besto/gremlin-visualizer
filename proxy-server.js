1817e76c848428f95c98f5275b8d87385297e7b4
Angelo Cisneros <acisneros@frontlineed.com>
2020-11-03 11:51:15 -0500

    Add environment variable configuration
    - GREMLIN_TRAVERSALSOURCE='g'
    - PROXY_PORT=5000
    - GREMLIN_HOST='localhost'
    - GREMLIN_PORT=8182
    - REST_SERVER=false - exposes the rest server to send raw queries

--- proxy-server.js
+++ proxy-server.js
@@ -9,8 +9,12 @@ const querystring = require("querystring");
 const path = require('path')
 const app = express();
 
-const port = 5000;
-const REST_SERVER = process.env.REST_SERVER || false;
+const GREMLIN_TRANSVERSALSOURCE = process.env.GREMLIN_TRANSVERSALSOURCE || 'g';
+const PROXY_PORT = +(process.env.PROXY_PORT || '5000');
+const GREMLIN_HOST = process.env.GREMLIN_HOST || 'localhost';
+const GREMLIN_PORT = +(process.env.GREMLIN_PORT || '8182');
+const port = PROXY_PORT;
+const REST_SERVER = process.env.REST_SERVER === 'true' || false;
 const logger = pino({ level: process.env.LOG_LEVEL || 'debug' });
 const expressLogger = expressPino({ logger });
 
@@ -41,6 +45,7 @@ function mapToObj(inputMap) {
 }
 
 function edgesToJson(edgeList) {
+    logger.debug('edgeList - before transformation: \n', edgeList);
     return edgeList.map(
         edge => ({
             id: typeof edge.get('id') !== "string" ? JSON.stringify(edge.get('id')) : edge.get('id'),
@@ -53,6 +58,7 @@ function edgesToJson(edgeList) {
 }
 
 function nodesToJson(nodeList) {
+    logger.debug('nodeList - before transformation: \n', nodeList);
     return nodeList.map(
         node => ({
             id: node.get('id'),
@@ -81,10 +87,14 @@ function makeQuery(query, nodeLimit) {
         "" +        ".fold())";                                            // node.edges[]
     return `${query}${nodeLimitQuery}${getNodesQuery}`
 }
+function makeQueryRaw(query, nodeLimit) {
+    const nodeLimitQuery = !isNaN(nodeLimit) && Number(nodeLimit) > 0 ? `.limit(${nodeLimit})` : '';
+    return `${query}`
+}
 
 app.post('/query', (req, res, next) => {
-    const gremlinHost = req.body.host;
-    const gremlinPort = req.body.port;
+    const gremlinHost = req.body.host || GREMLIN_HOST;
+    const gremlinPort = req.body.port || GREMLIN_PORT;
     const nodeLimit = req.body.nodeLimit;
     let query = req.body.query;
     if (query === "" || !query) {
@@ -117,6 +127,89 @@ app.post('/query', (req, res, next) => {
 
         request(url, {json: true}, (restError, restResponse, restBody) => {
             logger.debug(restResponse);
+            console.log('restResponse', restResponse);
+            if (restError) {
+                logger.warn(restResponse);
+                next(restError);
+                return;
+            }
+            if (restResponse.statusCode !== 200) {
+                logger.warn(restResponse.body.message);
+                logger.debug(restResponse.body);
+                const errorJson = {message: restResponse.body.message};
+                res.status(500).send(errorJson);
+                return;
+            }
+            res.send(restBody.result.data);
+        });
+    }
+});
+
+
+app.post('/q', async (req, res, next) => {
+    const gremlinHost = req.body.host || GREMLIN_HOST;
+    const gremlinPort = req.body.port || GREMLIN_PORT;
+    const nodeLimit = req.body.nodeLimit;
+    let query = req.body.query;
+    if (query === "" || !query) {
+        query = "g.V()"
+    }
+
+    logger.debug(`original query ${query}`);
+    const realQuery = makeQueryRaw(query, nodeLimit);
+    logger.debug(`real query ${realQuery}`);
+
+    if (!REST_SERVER) {
+        const client = new gremlin.driver.Client(`ws://${gremlinHost}:${gremlinPort}/gremlin`, {
+            traversalSource: GREMLIN_TRANSVERSALSOURCE,
+            mimeType: 'application/vnd.gremlin-v3.0+json'
+        });
+
+        try {
+            const result = await client.submit(realQuery, {});
+            logger.info("received result from query");
+            logger.debug('result: ', result);
+            function mapToJSON ( map ) {
+                let returnObj = {};
+                let entry, elementName, val;
+                for ( [entry] of map ) {
+                    if ( typeof entry === 'string' ) {
+                        returnObj[ entry ] = v;
+                    } else {
+                        [ {elementName}, val ] = entry;
+                        returnObj[ elementName ] = val;
+                        // returnObj[ k[0].elementName ] = v[1];
+                    }
+                }
+
+                // for (let [k, [ some, val]] of map) {
+                //     if ( typeof k === 'string' ) {
+                //         returnObj[ k ] = v;
+                //     } else {
+                //         returnObj[ k.elementName ] = v[1];
+                //         // returnObj[ k[0].elementName ] = v[1];
+                //     }
+                // }
+            
+                return returnObj;
+            }
+            let items = mapToJSON(result)
+
+            res.send({
+                items,
+                // items: nodesToJson(result._items)
+            })
+        } catch (err) {
+            logger.warn(err);
+            next(err)
+        }
+    } else {
+        const safeQuery = querystring.escape(realQuery);
+        const url = `http://${gremlinHost}:${gremlinPort}?gremlin=${safeQuery}`;
+
+        request(url, {json: true}, (restError, restResponse, restBody) => {
+            logger.debug(restResponse);
+            console.log('restResponse', restResponse);
             if (restError) {
                 logger.warn(restResponse);
                 next(restError);
